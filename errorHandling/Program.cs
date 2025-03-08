29d6a1f6d01c2752e8ed711b1616377167f7beb6
Sonu Lodhi <sp95759201@gmail.com>
2025-03-08 10:55:15 +0530

    practice

---
+++ errorHandling/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program{
+    static void Main(string[] args){
+        try{
+            Console.WriteLine("every thing looks good:");
+            int num1 = 10;
+            int num2 = 0;
+            Console.WriteLine(num1/num2);
+        }
+        catch(Exception e){
+            Console.WriteLine(e.ToString());
+        }
+        finally{
+            Console.WriteLine("Program end:");
+        }
+    }
+}
