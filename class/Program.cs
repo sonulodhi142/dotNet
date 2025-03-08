2d3ce0e6d507ebb1fe52d9953a71b8b2ead88019
Sonu Lodhi <sp95759201@gmail.com>
2025-03-08 10:10:11 +0530

    practice

---
+++ class/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Dog{
+
+    public string DogName = "";
+    public void sound(){
+        Console.WriteLine("I am a parent Class: "+DogName);
+    }
+}
+
+class Cat : Dog{
+    public string CatName = "";
+    public void Meaw(){
+        Console.WriteLine("I am a child Class: "+CatName);
+    }
+}
+
+class Cow : Cat{
+    public string CowName = "";
+    public void cow1(){
+        Console.WriteLine("i am a child of child Class: "+CowName);
+    }
+}
+
+class Program{
+    static void Main(string[] args){
+
+        Cow cow= new Cow();
+
+        cow.DogName = "Puppy";
+        cow.CatName = "chinmayi";
+        cow.CowName = "Rani";
+
+        cow.sound();
+        cow.Meaw();
+        cow.cow1();
+
+    }
+}
