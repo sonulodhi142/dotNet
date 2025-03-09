9b6627c4558d292588961db442b2472dc272a285
Sonu Lodhi <sp95759201@gmail.com>
2025-03-09 16:45:00 +0530

    add new application

--- MyCSharpApp/obj/Debug/net9.0/MyCSharpApp.AssemblyInfo.cs
+++ MyCSharpApp/obj/Debug/net9.0/MyCSharpApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71557b58ca972753fc096bcab9ff1da6a9652c74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29d6a1f6d01c2752e8ed711b1616377167f7beb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
