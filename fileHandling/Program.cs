ad5ea98f0cd90ae0d82ae95fb89325ad89940a2a
Sonu Lodhi <sp95759201@gmail.com>
2025-03-09 17:02:35 +0530

    create file

--- fileHandling/Program.cs
+++ fileHandling/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+class Program{
+    static void Main(string[] args){
+
+        string file = "file.txt";
+
+        File.WriteAllText(file,"\nthis the new file with the name of file.txt");
+        
+
+        Console.WriteLine(file);
+    }
+}
