d749b1729fc2e9f6d7be9a00cc203baf97ae141e
Sonu Lodhi <sp95759201@gmail.com>
2025-03-12 14:14:01 +0530

    add linq app

---
+++ Linq/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program {
+    static void Main(string[] args) {
+
+        List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9,};
+        // using loop itration
+        for(int i = 0; i < list.Count; i++){
+            Console.WriteLine(list[i]);
+        }
+
+        // using LINQ
+        var result = from num in list
+                     where num > 4
+                     select num;
+
+        foreach (int num in result) {
+            Console.WriteLine(num);
+        }
+    }
+}
