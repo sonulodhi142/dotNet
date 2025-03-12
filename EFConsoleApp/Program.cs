d75624c815e89835c8693b2567dce985517d6934
Sonu Lodhi <sp95759201@gmail.com>
2025-03-12 14:40:19 +0530

    connection db and code

--- EFConsoleApp/Program.cs
+++ EFConsoleApp/Program.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using Microsoft.EntityFrameworkCore;
 
 // (Represents Database Table)
-public class Student
-{
+public class Student{
     public int ID { get; set; }
     public string Name { get; set; }
     public int Age { get; set; }
+}
+
+// Bridge betwenn db and code
+public class SchoolContext : DbContext{
+    public DbSet<Student> Students { get; set;}; // Table Creation
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+        optionsBuilder.UseSqlite("Data source = school.db"); // data base connection
+    }
 }
