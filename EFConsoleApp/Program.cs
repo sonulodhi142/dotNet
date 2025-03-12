8e908c2aca0f322274803452b1667f081ba7d7dd
Sonu Lodhi <sp95759201@gmail.com>
2025-03-12 14:34:54 +0530

    db table

--- EFConsoleApp/Program.cs
+++ EFConsoleApp/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+// (Represents Database Table)
+public class Student
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
