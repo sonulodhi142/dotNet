6314d463641574e0bc28ee8c406089d32bdcfa97
Sonu Lodhi <sp95759201@gmail.com>
2025-03-15 17:36:43 +0530

    add

--- vote/Program.cs
+++ vote/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program{
+    static void Main(string[] args){
+
+        try{
+            Console.Write("Please Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if(age >= 18){
+                Console.WriteLine("You are eligible to vote:");
+            }
+            else{
+                Console.WriteLine("\n\nOops! You are not eligible to vote:");
+            }
+        }
+        catch(Exception ex){
+            Console.WriteLine(ex.ToString());
+        }
+    }
+}
