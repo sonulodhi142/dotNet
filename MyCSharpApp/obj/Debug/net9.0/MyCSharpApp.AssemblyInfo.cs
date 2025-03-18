41cef41df24a5d409d6f243e253265546c14a398
Sonu Lodhi <sp95759201@gmail.com>
2025-03-18 08:52:30 +0530

    two number swap code

--- MyCSharpApp/obj/Debug/net9.0/MyCSharpApp.AssemblyInfo.cs
+++ MyCSharpApp/obj/Debug/net9.0/MyCSharpApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+109281799a15c4856d6550b9555cfc5b0de849b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72c96627b7220a6ca1e6f554a8a9922043c7640e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- vote/Program.cs
+++ vote/Program.cs
@@ -1,27 +1,29 @@
 ﻿using System;
 
-class Program{
-    static void Main(string[] args){
+class Program
+{
+    static void Main(string[] args)
+    {
 
-        try{
-            Console.Write("Please Enter your age: ");
-            int age = Convert.ToInt32(Console.ReadLine());
-            if(age >= 18){
-                Console.WriteLine("\n\nYou are eligible to vote:\n");
-            }
-            else{
-                Console.WriteLine("\n\nOops! You are not eligible to vote:\n");
-            }
+        // try{
+        //     Console.Write("Please Enter your age: ");
+        //     int age = Convert.ToInt32(Console.ReadLine());
+        //     if(age >= 18){
+        //         Console.WriteLine("\n\nYou are eligible to vote:\n");
+        //     }
+        //     else{
+        //         Console.WriteLine("\n\nOops! You are not eligible to vote:\n");
+        //     }
 
-            int[] array = { 1, 2, 3,};
-            Console.WriteLine(array[age]);
-        }
-        catch(IndexOutOfRangeException ex){
-            Console.WriteLine("Error: Array index is out of range.");
-        }
-        catch(Exception ex){
-            Console.WriteLine(ex.ToString());
-        }
+        //     int[] array = { 1, 2, 3,};
+        //     Console.WriteLine(array[age]);
+        // }
+        // catch(IndexOutOfRangeException ex){
+        //     Console.WriteLine("Error: Array index is out of range.");
+        // }
+        // catch(Exception ex){
+        //     Console.WriteLine(ex.ToString());
+        // }
 
         // Console.Write("Enter Your age: ");
         // int age = Convert.ToInt32(Console.ReadLine());
@@ -49,6 +51,31 @@ class Program{
 
         //     Console.WriteLine("\n\nthe code is end:");
         // }
+
+        try
+        {
+            Console.Write("enter the num1: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the num2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nBefore Swaping the value:- \n");
+            Console.WriteLine($"num1 = {num1} \n num2 = {num2}");
+
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+
+            Console.WriteLine("\n After Swaping the value:- \n");
+            Console.WriteLine($"num1 = {num1} \n num2 = {num2}");
+        }
+        catch (Exception ex){
+            Console.WriteLine("Error:"+ex.Message);
+        }
+        finally{
+            Console.WriteLine("code end::-");
+        }
 
 
 
