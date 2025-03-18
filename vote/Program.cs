72c96627b7220a6ca1e6f554a8a9922043c7640e
Sonu Lodhi <sp95759201@gmail.com>
2025-03-18 08:44:54 +0530

    code

--- vote/Program.cs
+++ vote/Program.cs
@@ -3,25 +3,25 @@
 class Program{
     static void Main(string[] args){
 
-        // try{
-        //     Console.Write("Please Enter your age: ");
-        //     int age = Convert.ToInt32(Console.ReadLine());
-        //     if(age >= 18){
-        //         Console.WriteLine("\n\nYou are eligible to vote:\n");
-        //     }
-        //     else{
-        //         Console.WriteLine("\n\nOops! You are not eligible to vote:\n");
-        //     }
+        try{
+            Console.Write("Please Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if(age >= 18){
+                Console.WriteLine("\n\nYou are eligible to vote:\n");
+            }
+            else{
+                Console.WriteLine("\n\nOops! You are not eligible to vote:\n");
+            }
 
-        //     int[] array = { 1, 2, 3,};
-        //     Console.WriteLine(array[age]);
-        // }
-        // catch(IndexOutOfRangeException ex){
-        //     Console.WriteLine("Error: Array index is out of range.");
-        // }
-        // catch(Exception ex){
-        //     Console.WriteLine(ex.ToString());
-        // }
+            int[] array = { 1, 2, 3,};
+            Console.WriteLine(array[age]);
+        }
+        catch(IndexOutOfRangeException ex){
+            Console.WriteLine("Error: Array index is out of range.");
+        }
+        catch(Exception ex){
+            Console.WriteLine(ex.ToString());
+        }
 
         // Console.Write("Enter Your age: ");
         // int age = Convert.ToInt32(Console.ReadLine());
@@ -32,6 +32,25 @@ class Program{
         // else{
         //     Console.WriteLine("you are eligible for vote");
         // }
+
+        // try{
+        //     Console.Write("enter the num1: ");
+        // int num1 = Convert.ToInt32(Console.ReadLine());
+
+        // Console.Write("Enter the num2: ");
+        // int num2 = Convert.ToInt32(Console.ReadLine());
+
+        // Console.WriteLine($"\n\nnum1 is {num1} and num2 is {num2}");
+        // }
+        // catch(Exception ex){
+        //     Console.WriteLine("\n\nError is :"+ex.Message);
+        // }
+        // finally{
+
+        //     Console.WriteLine("\n\nthe code is end:");
+        // }
+
+
 
 
     }
