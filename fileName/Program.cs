2e497318d416520c0cb7718ab37b9e3c70901072
Sonu Lodhi <sp95759201@gmail.com>
2025-03-19 15:15:29 +0530

    filehangling

---
+++ fileName/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.IO;
+class Program{
+    static void Main(string[] args){
+        string path = "example.txt";
+        using (StreamWriter fs = new StreamWriter(path)){
+            fs.Write("\nthis is the text of that i wrote in this file");
+        }
+    }
+}
