3221d720042ea4452c2d054cee92216e9d4fd661
Sonu Lodhi <sp95759201@gmail.com>
2025-03-22 14:12:04 +0530

    add some oprations

--- students/Program.cs
+++ students/Program.cs
@@ -1,48 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class Program{
-    static void Main(string[] args){
+class Program
+{
+    static void Main(string[] args)
+    {
 
         List<Dictionary<string, object>> students = new List<Dictionary<string, object>>();
+        int studentId = 1;
 
-        Dictionary<string,object> student = new Dictionary<string, object>();
-
-        while(true){
+        while (true)
+        {
+            Console.WriteLine("\n\nEnter 1 to view all students:");
+            Console.WriteLine("Enter 2 to add student :");
+            Console.WriteLine("Enter 4 to delete student :");
+            Console.WriteLine("Enter 5 to exit:: ");
             Console.Write("enter the choice :: ");
             int choice = Convert.ToInt32(Console.ReadLine());
 
-            switch(choice){
+            switch (choice)
+            {
                 case 1:
                     viewStudents();
                     break;
                 case 2:
-                    viewStudents();
+                    addStudents();
                     break;
                 case 3:
                     viewStudents();
                     break;
                 case 4:
-                    viewStudents();
+                    deleteStudent();
                     break;
                 case 5:
                     return;
-                
+
             }
         }
 
-        
 
-        void viewStudents(){
-            student["id"] = 1;
-        student["name"] = "sonu";
-        student["grade"] = "A";
 
-        students.Add(student);
-            Console.WriteLine(students.Count);
-            foreach(var stud in students){
-                Console.WriteLine((int)stud["id"]==1);
+        void viewStudents()
+        {
+            int id = 1;
+            Console.WriteLine("The number of Total student is :" + students.Count);
+            foreach (var stud in students)
+            {
+                Console.WriteLine("\n\nId    :: " + stud["id"]);
+                Console.WriteLine("Name  :: " + stud["name"]);
+                Console.WriteLine("Grade :: " + stud["grade"]);
+                Console.WriteLine((int)stud["id"] == id);
             }
         }
+
+        void addStudents()
+        {
+
+            Dictionary<string, object> student = new Dictionary<string, object>();
+            Console.WriteLine("Enter the Student basic Details");
+            student["id"] = studentId++;
+            Console.Write("Name:: ");
+            student["name"] = Console.ReadLine();
+            Console.Write("Grade:: ");
+            student["grade"] = Console.ReadLine();
+
+            students.Add(student);
+            Console.WriteLine("The student is Succesfully added to the list:-");
+        }
+
+        void deleteStudent()
+        {
+            Console.Write("Enter the Id of Student::");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= students.Count - 1; i++) // Iterate in reverse
+            {
+                if ((int)students[i]["id"] == id)
+                {
+                    students.RemoveAt(i);
+                    Console.WriteLine("Student deleted successfully.");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Student not found.");
+        }
+
     }
 }
