837abcfaa6e3d82380bda9d0cc8487b9304b633f
Sonu Lodhi <sp95759201@gmail.com>
2025-03-22 11:54:21 +0530

    check concepts

--- students/Program.cs
+++ students/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program{
+    static void Main(string[] args){
+
+        List<Dictionary<string, object>> students = new List<Dictionary<string, object>>();
+
+        Dictionary<string,object> student = new Dictionary<string, object>();
+
+        student["id"] = 1;
+        student["name"] = "sonu";
+        student["grade"] = "A";
+
+        students.Add(student);
+
+        Console.WriteLine(students.Count);
+        foreach(var stud in students){
+            Console.WriteLine((int)stud["id"]==1);
+        }
+    }
+}
