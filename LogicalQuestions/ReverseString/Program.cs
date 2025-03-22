a1466dc848077940be23cdad614b73e1849b517e
Sonu Lodhi <sp95759201@gmail.com>
2025-03-22 21:47:55 +0530

    ReverseString

---
+++ LogicalQuestions/ReverseString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program{
+    static void Main(string[] args){
+        Console.Write("Enter any string::");
+        string str = Console.ReadLine();
+
+        
+
+        for(int i = str.Length-1; i >= 0 ; i--){
+            Console.Write(str[i]);
+        }
+    }
+}
