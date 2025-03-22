3881e1612a221adb227cdb18e47f42b186f47b1f
Sonu Lodhi <sp95759201@gmail.com>
2025-03-22 21:31:58 +0530

    Factorial

---
+++ LogicalQuestions/Factorial/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program{
+    static void Main(string[] args){
+
+        Console.Write("Enter a number:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int Factorial = 1;
+
+        for(int i = 1; i <= num; i++){
+            Factorial = Factorial * i;
+        }
+
+        Console.WriteLine($"Factorial of {num}  is :{Factorial}");
+    }
+}
