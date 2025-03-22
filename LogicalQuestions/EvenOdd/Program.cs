b9dad74e1a48fbc9b127cfc7ac60dc9c49b90a67
Sonu Lodhi <sp95759201@gmail.com>
2025-03-22 21:16:22 +0530

    evenOdd

---
+++ LogicalQuestions/EvenOdd/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Porgam{
+    public static void Main(string[] args){
+
+        Console.Write("Enter the Number:: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if(num%2==0){
+            Console.WriteLine("The given number is even::");
+        }
+        else{
+            Console.WriteLine("The given number is odd::");
+        }
+    }
+}
