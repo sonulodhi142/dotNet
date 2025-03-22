6df575fd5f2e971802de0b3759841dc63caae242
Sonu Lodhi <sp95759201@gmail.com>
2025-03-22 21:24:13 +0530

    SumofNTerms

---
+++ LogicalQuestions/SumOfTenNum/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program{
+    static void Main(string[] args){
+
+        Console.Write("Enter a number:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int sum = 0;
+
+        for(int i = 0; i <= num; i++){
+            sum = sum + i;
+        }
+
+        Console.WriteLine($"Sum of {num} numers is :{sum}");
+    }
+}
