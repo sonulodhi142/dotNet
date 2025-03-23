de8f3750abafa63e0e422bf1797b18d21f6888c0
Sonu Lodhi <sp95759201@gmail.com>
2025-03-23 12:52:03 +0530

    remove duplicate element in array

--- LogicalQuestions/duplicateInArray/Program.cs
+++ LogicalQuestions/duplicateInArray/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 2, 3, 4, 4, 5 };
+        
+        HashSet<int> set = new HashSet<int>(array);
+        
+        foreach(int i in set){
+            Console.WriteLine(i);
+        }
+    }
+}
