68281ad5f0f344ee3b5d31b080541d5ef15d8ec8
Sonu Lodhi <sp95759201@gmail.com>
2025-03-23 13:00:55 +0530

    code

--- LogicalQuestions/largeSmallInArray/Program.cs
+++ LogicalQuestions/largeSmallInArray/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program {
+    static void Main(string[] args) {
+        int[] array = { 1, 2, 3, 4, 5, 10, 99, 67, 47, 88, 0, 777};
+
+        int largest = array[0];
+        int smallest = array[0];
+
+        foreach (int i in array) {
+            if(smallest > i){
+                smallest = i;
+            }
+            if(largest < i){
+                largest = i;
+            }
+        }
+        Console.WriteLine("the Largest number in array is :" + largest);
+        Console.WriteLine("the smallest number in array is :" + smallest);
+    }
+}
