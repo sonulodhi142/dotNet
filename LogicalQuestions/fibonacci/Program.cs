a54d9a9adbfc25a3602736810c6d83aacbb84fb1
Sonu Lodhi <sp95759201@gmail.com>
2025-03-24 12:24:14 +0530

    code

--- LogicalQuestions/fibonacci/Program.cs
+++ LogicalQuestions/fibonacci/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Enter the number of terms::");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        int firstNum = 0;
+        int secNum = 1;
+        int thirdNum = 0;
+        Console.WriteLine(firstNum);
+        Console.WriteLine(secNum);
+
+        for (int i = 2; i < num; i++) {
+            thirdNum = firstNum + secNum;
+            firstNum = secNum;
+            secNum = thirdNum;
+            Console.WriteLine(thirdNum);
+        }
+
+    }
+}
