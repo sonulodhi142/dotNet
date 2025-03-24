9765c7d7c2f479906de3d092b08054e46c52a5f2
Sonu Lodhi <sp95759201@gmail.com>
2025-03-24 11:44:31 +0530

    add prime number program

--- LogicalQuestions/duplicateInArray/Program.cs
+++ LogicalQuestions/duplicateInArray/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main()
     {
-        int[] array = { 1, 2, 2, 3, 4, 4, 5 };
+        int[] array = { 1, 2, 2, 3, 4, 4, 5, 6, 7, 7, 1, 5, 0 };
         
         HashSet<int> set = new HashSet<int>(array);
         
---
+++ LogicalQuestions/primeNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Enter the number:");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 2; i < num; i++) {
+            if(num%i==0){
+                Console.WriteLine("The given number is Not prime:");
+                return;
+            }
+        }
+        Console.WriteLine("The given number is Prime::");
+    }
+}
