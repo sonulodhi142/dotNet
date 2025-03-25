ffb4713208782b35078761af184cd73e0c0cbadb
Sonu Lodhi <sp95759201@gmail.com>
2025-03-25 10:03:48 +0530

    add countprime app

---
+++ LogicalQuestions/starPatter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class program {
+    static void Main(string[] args){
+        Console.Write("Enter the Number line ::");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        string star = "* ";
+        Console.WriteLine("Star pattern:-");
+        for (int i = 0; i < num; i++) {
+            Console.WriteLine(star);
+            star = star + "* ";
+        }
+    }
+}
