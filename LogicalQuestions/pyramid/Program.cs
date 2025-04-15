2650741219c5d98baf9ab392b6306970ff8b5807
Sonu Lodhi <sp95759201@gmail.com>
2025-04-15 11:02:13 +0530

    add code

--- LogicalQuestions/pyramid/Program.cs
+++ LogicalQuestions/pyramid/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program{
+    static void Main(string[] args){
+        Console.Write("Enter the num::");
+        int num = Convert.ToInt32(Console.ReadLine());
+        for(int i = 0; i < num; i++){
+            for(int j = 0; j < num - i; j++){
+                Console.Write(" ");
+            }
+            for(int k = 0; k < i; k++){
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
